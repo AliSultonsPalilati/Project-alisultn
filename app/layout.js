bb736652a3a59eb76fc32739e5121bffba1e3589
AliSultonsPalilati <105841102222@student.unismuh.ac.id>
2024-08-16 08:58:16 +0800

    Commit Pertama

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,22 @@
-export default function RootLayout({ children }) {
+import { Lusitana } from 'next/font/google';
+import SideNav from '@/app/ui/dashboard/sidenav';
+
+const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
+
+export default function Layout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${lusitana.className} antialiased`}>{children}
+      <div className="flex flex-col h-screen md:flex-row md:overflow-hidden">
+      <div className="flex-none w-full md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+      </body>
     </html>
   );
 }
